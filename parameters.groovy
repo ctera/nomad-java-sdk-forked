cb14d71baf7aff16ddf444faccc09cd4e0b5eea9
Keynan Levi <keynanl@ctera.com>
2024-02-22 23:01:57 +0200

    debug

--- parameters.groovy
+++ parameters.groovy
@@ -1,8 +1,7 @@
 public inputParameters() {
     def parameters = [
         choice(name: "testSkip", choices: ['true', 'false'], description: "Skip the execution of tests during the Maven build process"),
-        choice(name: "modernizerSkip", choices: ['true', 'false'], description: "Skip the execution of the modernizer plugin during the Maven build process"),
-        string(name: "pomPath", defaultValue: "./sdk/pom.xml", description: "The full path to pom.xml which get updated by the pipeline")
+        choice(name: "modernizerSkip", choices: ['true', 'false'], description: "Skip the execution of the modernizer plugin during the Maven build process")
     ]
     return parameters
 }
