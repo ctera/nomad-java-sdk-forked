92155f4ba55a430c6c90146c01a65dc464850bb1
Keynan Levi <keynanl@ctera.com>
2024-02-22 12:04:49 +0200

    pmPath

--- pipeline-configuration/parameters.groovy
+++ pipeline-configuration/parameters.groovy
@@ -2,7 +2,7 @@ public inputParameters() {
     def parameters = [
         choice(name: "testSkip", choices: ['true', 'false'], description: "Skip the execution of tests during the Maven build process"),
         choice(name: "modernizerSkip", choices: ['true', 'false'], description: "Skip the execution of the modernizer plugin during the Maven build process"),
-        string(name: "pomLocation", defaultValue: "./sdk/pom.xml", description: "The full path to pom.xml which get updated by the pipeline")
+        string(name: "pomPath", defaultValue: "./sdk/pom.xml", description: "The full path to pom.xml which get updated by the pipeline")
     ]
     return parameters
 }
