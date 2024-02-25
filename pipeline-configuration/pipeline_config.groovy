12f9a4788dc9d89f3768317a8937f6725f8096d8
Keynan Levi <keynanl@ctera.com>
2024-02-25 10:39:49 +0200

    remove comments

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,18 +1,5 @@
-// jte{
-//     pipeline_template = "Jenkinsfile_rpm"
-// }
-
 libraries{    
     maven{
        pom_full_name = "sdk/pom.xml"
     }    
 }
-
-// stages{
-//     _continuous_integration{
-//         _are_changes_detected
-//         _build
-//         _copy_rpm_and_update_oss
-//     }
-// }
-
